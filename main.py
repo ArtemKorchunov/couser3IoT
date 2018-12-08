dafe8b69bbdcd23d75c2ac7f0fb80550d18ab704
Artem Korchunov <artem.korchunof@gmail.com>
2018-12-08 21:31:06 +0200

    iot add integration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import sys
+import Adafruit_DHT
 
 print(__name__)
 
@@ -8,7 +10,7 @@ def main():
     content_current = f.read()
     f.close()
     if content_current == "":
-        res = requests.post('http://localhost:4000/iot/auth',
+        res = requests.post('http://192.168.0.108:4000/iot/auth',
                             data={'name': 'raspberry'})
         print(res)
         if res.status_code == 201:
@@ -18,8 +20,12 @@ def main():
             f.write(res_payload_dict['data']['identifier'])
             f.close()
     if content_current:
-        requests.get('http://localhost:4000/iot/logger',
-                     params={'heat': 25, 'identifier': content_current})
+	while True:
+		humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+		print("Temp: {} C  Humidity: {}".format(temperature, humidity))
+		requests.get('http://192.168.0.108:4000/iot/logger',
+			params={'heat': temperature, 'identifier': content_current})
 
 
 if __name__ == "__main__":
