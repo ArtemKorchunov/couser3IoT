fa4a3a4fc05581b22f114f6441fc188d770255c8
ArtemK <artem.korchunof@gmail.com>
2018-12-09 12:51:55 +0200

    fix

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ print(__name__)
 def main():
     content_current = {'identifier': ''}
     f = open("creds.txt", "r")
-    fread = f.read()
-    if fread:
-        content_current = json.loads(fread)
+    content_current['identifier'] = f.read()
     f.close()
-    res = requests.post('http://192.168.0.108:4000/iot/auth',
+    res = requests.post('http://http://192.168.0.108:4000/iot/auth',
                         data={'name': 'raspberry', 'identifier': content_current['identifier']})
     print(res.text)
     if res.status_code == 201:
         res_payload_dict = res.json()
-        content_current = res_payload_dict['data']['identifier']
-        with open('creds.txt', 'w+') as outfile:
-            json.dump(res_payload_dict['data'], outfile)
+        content_current = res_payload_dict['data']
+        f = open("creds.txt", "w+")
+        f.write(res_payload_dict['data']['identifier'])
+        f.close()
+
     if content_current:
         while True:
             humidity, temperature = Adafruit_DHT.read_retry(11, 4)
